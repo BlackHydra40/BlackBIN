399bcb1205e04ba09068f8e1e48f7dd7912d4354
BLACK_HYDRA <112259891+BlackHydra40@users.noreply.github.com>
2022-10-07 11:04:44 -0300

    up.sh

--- up.sh
+++ up.sh
@@ -6,6 +6,7 @@ figlet "AGUARDE..." | lolcat
 figlet "ABRINDO GIT HUB" | lolcat
 am start -a android.intent.action.VIEW -d https://github.com/BlackHydra40/BlackBIN > /dev/null 2>&1
 sleep 5
+clear
 figlet "ABRINDO TELEGRAM" | lolcat
 am start -a android.intent.action.VIEW -d https://t.me/MS40_canal > /dev/null 2>&1
 sleep 3
